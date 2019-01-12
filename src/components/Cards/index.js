b8f6e4a002ba70be8108b160e1999518d25a85c0
LamarC <dviann89@gmail.com>
2019-01-12 09:36:57 -0600

    Cards are showing now

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -5,7 +5,7 @@ const Cards = (props) => {
     return (
     <div className='card'>
         <div className='img-container'>
-            <img alt={props.name} src={props.image} onClick={() => props.clickCount(props.id)} />      
+            <img alt={props.name} src={props.image} />      
         </div>
 
         {/* <span onClick={() => props.removeCards(props.id)} className='remove'></span> */}
