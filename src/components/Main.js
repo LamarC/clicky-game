8a9e3fed348e6998dbe23cbebd75777d2618ffb9
LamarC <dviann89@gmail.com>
2019-01-21 00:16:22 -0600

    Error on the main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-
-import Card from "./Card";
+import Cards from "./Cards";
 import Score from "./Score";
 
 const array = [
@@ -68,21 +67,50 @@ const array = [
 
 const shift = (arr) => (
   arr
-      .map((a) => [Math.random(), a])
-      .sort((a, b) => a[0] - b[0])
-      .map((a) => a[1])
+    .map((a) => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map((a) => a[1])
 );
 
-class Game extends Component {
+class Main extends Component {
 
   state = {
-      score: 0,
-      characters: shift(array)
+    score: 0,
+    chars: shift(array)
   };
+
+};
+
+onCharClick = (index) => {
+  if (!this.state.chars[index].clicked) {
+    this.setState({
+      chars: shift(this.state.chars.map((char, current) => {
+        return (current === index) ? { ...char, clicked: true } : char
+      })),
+      score: this.state.score + 1
+    });
+
+  } else {
+    this.setState({
+      chars: shift(this.state.chars.map((char) => { return { ...char, clicked: false } })),
+      score: 0
+    });
+  }
 }
 
-render() {
-  return ()
+render(){
+  return(
+    <div className="main">
+      <header className="header">
+        <h1>Clicky Game</h1>
+        <h2>Click on a card without choosing the same more than once</h2>
+
+        <Score 
+        score={this.state.score}/>
+      </header>
+    </div>
+  )
 }
+
 
 export default Main;
